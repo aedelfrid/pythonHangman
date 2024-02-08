408e42d81bbb8af49eca0d1eccadacc878109bfb
aedelfrid <almck51@gmail.com>
2024-02-08 02:46:20 -0400

    created basic start for hangman game

--- main.py
+++ main.py
@@ -1,1 +1,47 @@
-print("Hello, World!")
+import random
+
+words = [
+    "TEST",
+    "PYTHON",
+    "VARIABLE",
+    "LOOP",
+    "ARRAY", 
+    "INTEGER",
+    "STRING",
+    "FLOAT", 
+    "OBJECT",
+    "CLASS"
+]
+
+class word:
+    def __init__(self):
+        self.w = words[random.randint(0, len(words) - 1)]
+
+    def __str__(self):
+        return self.w
+
+    def breakDownWord(self) :
+        return list(self.w)
+
+    def hideWord(self) :
+        self.currentWord = self.w
+
+        for i in self.currentWord:
+            self.currentWord[i] = "_"
+
+    def revealLetter(self, guess) :
+        for i in self.w:
+            if self.w[i] == guess:
+                self.currentWord[i] = guess
+            else:
+                player.lives - 1
+
+
+class player:
+    def __init__(self, name) :
+        self.name = name
+        self.lives = 5
+        self.score = 0
+
+name = input("Thanks for playing Hangman!\nPlease enter a name for our scoreboard!")
+
--- planning.md
+++ planning.md
@@ -2,9 +2,16 @@
 
 generate random word from list
 
-generate ascii art?
+    generate ascii art?
 
 display word as blanks
+    break down into individual letters
+    replace with underscore
+        chosenWord = ["T", "E", "S", "T"]
+        currentWord = ["_", "_", "_", "_"]
+    for i in chosenWord:
+        if chosenWord[i] == input
+            currentWord[i] = input
 
 ask for input
 
