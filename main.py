c7c6e1d615f990010e79b2b62d88a9166b9b1bcd
aedelfrid <almck51@gmail.com>
2024-02-08 18:42:42 -0400

    added start of gameplay loop

--- main.py
+++ main.py
@@ -10,12 +10,15 @@ words = [
     "STRING",
     "FLOAT", 
     "OBJECT",
+    "STRING CONCATENATION",
     "CLASS"
 ]
 
 class word:
     def __init__(self):
         self.w = words[random.randint(0, len(words) - 1)]
+        self.currentWord = self.w
+        self.guessedChars = []
 
     def __str__(self):
         return self.w
@@ -24,17 +27,18 @@ class word:
         return list(self.w)
 
     def hideWord(self) :
-        self.currentWord = self.w
-
         for i in self.currentWord:
-            self.currentWord[i] = "_"
+            if self.currentWord[i] != " ":
+                self.currentWord[i] = "_"
 
-    def revealLetter(self, guess) :
+    def revealLetter(self, user, guess) :
         for i in self.w:
             if self.w[i] == guess:
                 self.currentWord[i] = guess
+                user.score += 1
             else:
-                player.lives - 1
+                self.guessedChars.append(guess)
+                user.lives -= 1
 
 
 class player:
@@ -43,5 +47,35 @@ class player:
         self.lives = 5
         self.score = 0
 
-name = input("Thanks for playing Hangman!\nPlease enter a name for our scoreboard!")
+def guessWord(chosenWord) :
+    print(f"HANGMAN\nGuess the Word!\n{chosenWord.currentWord}\nGuessed letters:\n{chosenWord.guessedChars}")
+    guess = input("Guess a letter.")
+
+    for i in chosenWord.guessedChars:
+        if guess == chosenWord.guessedChars[i]:
+            print(f"Already guessed {guess}, please try again.")
+            guessWord(chosenWord)
+
+    chosenWord.revealLetter(guess)
+
+def main() :
+
+    user = player(input("Thanks for playing Hangman!\nPlease enter a name for our scoreboard!\n"))
+    chosenWord = word()
+
+    chosenWord.breakDownWord()
+    # need to break down current word without breaking down chosen word?
+    chosenWord.hideWord()
+
+    for i in range(26):
+        if user.lives == 0:
+            print("You have run out of guesses.\nGAME OVER")
+            quit()
+
+        if chosenWord.currentWord ==
+        guessWord(chosenWord)
+
+# main()
+
+
 
